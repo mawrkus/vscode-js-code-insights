9a1bfd016851a756ee802bd9688ae95ecae9eeb0
mawrkus <web@sparring-partner.be>
2021-10-30 17:26:37 +0200

    feat(*): Added markdown document creation

--- extension.js
+++ extension.js
@@ -1,30 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const computeIdentifierDensity = require('./lib/computeIdentifierDensity');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const { logger } = require("./lib/logger");
+const { computeIdentifierDensity } = require('./lib/computeIdentifierDensity');
+const { createMarkdownDocument } = require('./lib/createMarkdownDocument');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('The JS Word Counter extension is now active');
+	// your extension is activated the very first time the command is executed
+	logger.log('Extension activated');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('js-word-counter.computeIdentifierDensity', function () {
+	let disposable = vscode.commands.registerCommand('js-word-counter.computeIdentifierDensity', async () => {
+		if (!vscode.window.activeTextEditor) {
+			return;
+		}
+
+		const { fileName } = vscode.window.activeTextEditor.document;
+
+		logger.log(`Processing "${fileName}"...`);
+
+		const code = vscode.window.activeTextEditor.document.getText();
+
 		try {
-			const result = computeIdentifierDensity('let solution = 42; ((value) => solution === value)(value);');
-			console.log(result);
+			const results = computeIdentifierDensity(code);
+
+			logger.log(`"${fileName}" results →`, results);
+
 			vscode.window.showInformationMessage('JS identifier density computed!');
+
+			const newDocument = await vscode.workspace.openTextDocument({
+				content: createMarkdownDocument({ fileName, results }),
+				language: 'markdown',
+			});
+
+			vscode.window.showTextDocument(newDocument);
 		} catch(error) {
-			console.error(error);
+			logger.error(error);
 			vscode.window.showErrorMessage(`Error while computing JS identifier density: ${error.message}!`);
 		}
 	});
@@ -32,7 +48,6 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
